1d769ee8ba5e695f117dabf6911722b0067461c3
wenyan4work <wenyan4work@gmail.com>
2017-09-21 09:16:58 +0800

    remove regularization of Stokes velocity kernel

--- include/pvfmm_common.hpp
+++ include/pvfmm_common.hpp
@@ -39,8 +39,6 @@ enum PeriodicType{
 
 extern PeriodicType periodicType;
 
-#define STOKESREGDELTA (0)
-
 #define RAD0 1.05 //Radius of upward equivalent (downward check) surface.
 #define RAD1 2.95 //Radius of downward equivalent (upward check) surface.
 
