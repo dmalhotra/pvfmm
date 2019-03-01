4693e4eec3e48cc86acf11e0a6e36b50bdd51e9b
Wen Yan <wenyan4work@gmail.com>
2019-02-28 21:00:26 -0500

    add mac configure script

---
+++ mac_clang_mkl_configure.sh
@@ -0,0 +1,11 @@
+unset PVFMM_DIR
+
+export CXXFLAGS="-std=c++14 -O3 -march=native -DFFTW3_MKL" 
+# embed rpath
+export LDFLAGS="-L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_rt -L/opt/intel/lib -Wl,-rpath,/opt/intel/lib -liomp5 "
+
+./configure --prefix=/Users/wyan/local/ \
+--with-openmp-flag="fopenmp=libiomp5" \
+--with-fftw-include="${MKLROOT}/include/fftw" \
+--with-fftw-lib="-lmkl_rt" 
+
