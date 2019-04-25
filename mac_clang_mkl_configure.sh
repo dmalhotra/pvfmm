064d02e48cdc40808da29f19cc86c0bb9195a761
Wen Yan <wenyan4work@gmail.com>
2019-04-25 08:09:51 -0400

    fopenmp=libiomp5 -> fopenmp

--- mac_clang_mkl_configure.sh
+++ mac_clang_mkl_configure.sh
@@ -2,10 +2,10 @@ unset PVFMM_DIR
 
 export CXXFLAGS="-std=c++14 -O3 -march=native -DPVFMM_FFTW3_MKL" 
 # embed rpath
-export LDFLAGS="-L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_rt -L/opt/intel/lib -Wl,-rpath,/opt/intel/lib -liomp5 "
+export LDFLAGS="-L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_rt"
 
 ./configure --prefix=/Users/wyan/local/ \
---with-openmp-flag="fopenmp=libiomp5" \
+--with-openmp-flag="fopenmp" \
 --with-fftw-include="${MKLROOT}/include/fftw" \
 --with-fftw-lib="-lmkl_rt" 
 
