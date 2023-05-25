32b8f07e5f9b545f9ead258dd488989306847de0
Brian Ward <bward@flatironinstitute.org>
2023-05-25 10:56:58 -0400

    Formatting

--- python/pvfmm.py
+++ python/pvfmm.py
@@ -18,6 +18,7 @@ class FMMKernel(Enum):
     PVFMMStokesVelocityGrad = 4
     PVFMMBiotSavartPotential = 5
 
+
 class FMMDoubleVolumeContext:
     def __init__(
         self,
@@ -36,4 +37,26 @@ class FMMDoubleVolumeContext:
         if hasattr(self, "_ptr"):
             ffi.PVFMMDestroyVolumeFMMD(ctypes.pointer(self._ptr))
 
+# SLOW
 # fmm = FMMContext(10,14, FMMKernel.PVFMMStokesVelocity, MPI.COMM_WORLD)
+
+class FFMDoubleParticleContext:
+    def __init__(
+        self,
+        box_size: int,
+        max_points: int,
+        multipole_order: int,
+        kernel: FMMKernel,
+        comm: MPI.Comm,
+    ):
+        if multipole_order <= 0 or multipole_order % 2 != 0:
+            raise ValueError("multipole order must be even and postive")
+        self._ptr = ffi.PVFMMCreateContextD(box_size,
+            multipole_order, max_points, int(kernel.value), ffi.get_MPI_COMM(comm)
+        )
+
+    def __del__(self):
+        if hasattr(self, "_ptr"):
+            ffi.PVFMMDestroyContextD(ctypes.pointer(self._ptr))
+
+# fmm = FFMDoubleParticleContext(-1, 1000,10,FMMKernel.PVFMMBiotSavartPotential, MPI.COMM_WORLD)
