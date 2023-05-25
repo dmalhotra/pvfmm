0c5a7e42662853e09caf92758ac2170d66185daa
Brian Ward <bward@flatironinstitute.org>
2023-05-25 10:48:07 -0400

    First sketch of Python wrapper

---
+++ python/pvfmm.py
@@ -0,0 +1,39 @@
+# module load gcc/11.3.0 openmpi python/3.10.8
+# source ~/venvs/pvfmm/bin/activate
+
+import ctypes
+import numpy as np
+from mpi4py import MPI
+from enum import Enum
+import ffi
+
+
+class FMMKernel(Enum):
+    """Mirroring PVFMMKernel in pvfmm.h"""
+
+    PVFMMLaplacePotential = 0
+    PVFMMLaplaceGradient = 1
+    PVFMMStokesPressure = 2
+    PVFMMStokesVelocity = 3
+    PVFMMStokesVelocityGrad = 4
+    PVFMMBiotSavartPotential = 5
+
+class FMMDoubleVolumeContext:
+    def __init__(
+        self,
+        multipole_order: int,
+        chebyshev_degree: int,
+        kernel: FMMKernel,
+        comm: MPI.Comm,
+    ):
+        if multipole_order <= 0 or multipole_order % 2 != 0:
+            raise ValueError("multipole order must be even and postive")
+        self._ptr = ffi.PVFMMCreateVolumeFMMD(
+            multipole_order, chebyshev_degree, int(kernel.value), ffi.get_MPI_COMM(comm)
+        )
+
+    def __del__(self):
+        if hasattr(self, "_ptr"):
+            ffi.PVFMMDestroyVolumeFMMD(ctypes.pointer(self._ptr))
+
+# fmm = FMMContext(10,14, FMMKernel.PVFMMStokesVelocity, MPI.COMM_WORLD)
