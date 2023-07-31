a670e133d94956e82780ce9c5584a477abca2bf4
Brian Ward <bward@flatironinstitute.org>
2023-07-31 17:21:32 -0400

    Add example based on example-c

--- python/pvfmm.py
+++ python/pvfmm.py
@@ -1,10 +1,15 @@
-# module load gcc/11.3.0 openmpi python/3.10.8
+# module load gcc/11.3.0 openmpi python/3.10.8 openblas fftw3
 # source ~/venvs/pvfmm/bin/activate
 
 import ctypes
 import numpy as np
-from mpi4py import MPI
 from enum import Enum
+from typing import Optional
+
+# calls MPI_Init_thread() and sets up MPI_Finalize() for you.
+# see https://mpi4py.readthedocs.io/en/stable/mpi4py.run.html
+from mpi4py import MPI
+
 import ffi
 
 
@@ -37,13 +42,11 @@ class FMMDoubleVolumeContext:
         if hasattr(self, "_ptr"):
             ffi.PVFMMDestroyVolumeFMMD(ctypes.pointer(self._ptr))
 
-# SLOW
-# fmm = FMMContext(10,14, FMMKernel.PVFMMStokesVelocity, MPI.COMM_WORLD)
 
 class FFMDoubleParticleContext:
     def __init__(
         self,
-        box_size: int,
+        box_size: float,
         max_points: int,
         multipole_order: int,
         kernel: FMMKernel,
@@ -51,12 +54,46 @@ class FFMDoubleParticleContext:
     ):
         if multipole_order <= 0 or multipole_order % 2 != 0:
             raise ValueError("multipole order must be even and postive")
-        self._ptr = ffi.PVFMMCreateContextD(box_size,
-            multipole_order, max_points, int(kernel.value), ffi.get_MPI_COMM(comm)
+        self._ptr = ffi.PVFMMCreateContextD(
+            float(box_size),
+            max_points,
+            multipole_order,
+            int(kernel.value),
+            ffi.get_MPI_COMM(comm),
         )
 
     def __del__(self):
         if hasattr(self, "_ptr"):
-            ffi.PVFMMDestroyContextD(ctypes.pointer(self._ptr))
+            ffi.PVFMMDestroyContextD(ctypes.pointer(ctypes.c_void_p(self._ptr)))
 
-# fmm = FFMDoubleParticleContext(-1, 1000,10,FMMKernel.PVFMMBiotSavartPotential, MPI.COMM_WORLD)
+    def evaluate(
+        self,
+        src_pos: np.ndarray,
+        sl_den: np.ndarray,
+        dl_den: Optional[np.ndarray],
+        trg_pos: np.ndarray,
+        setup: bool,
+    ):
+        source_length = len(src_pos)
+        if source_length % 3 != 0:
+            raise ValueError(
+                "Source arrays must have a length which is a multiple of 3"
+            )
+        if dl_den is not None and len(sl_den) != source_length:
+            raise ValueError("Source arrays must all be of the same length!")
+        if dl_den is not None and len(dl_den) != source_length * 2:
+            raise ValueError("Source arrays must all be of the same length!")
+        n_src = source_length // 3
+
+        target_length = len(trg_pos)
+        if target_length % 3 != 0:
+            raise ValueError(
+                "Target arrays must have a length which is a multiple of 3"
+            )
+        n_trg = target_length // 3
+        trg_val = np.empty(target_length)
+
+        ffi.PVFMMEvalD(
+            src_pos, sl_den, dl_den, n_src, trg_pos, trg_val, n_trg, self._ptr, int(setup)
+        )
+        return trg_val
